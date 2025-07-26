247e03b3e416989210906a8cf3af6a9a940ce19b
Shant <shant212205@gmail.com>
2025-07-26 12:59:35 -0800

    update

--- components/AboutHero/index.jsx
+++ components/AboutHero/index.jsx
@@ -32,13 +32,14 @@ export default function AboutHero() {
           </AboutParagraph>
         </Content>
         <AboutVideo
+          src="https://ccix1anvvnzyhdq1.public.blob.vercel-storage.com/video.mp4"
+          type="video/mp4"
           autoPlay
           muted
           loop
           playsInline
-          preload="auto">
-          <source src="/videos/video.mp4" type="video/mp4" />
-        </AboutVideo>
+          preload="auto"
+        />
       </AboutContainer>
     </AboutSection>
   );
